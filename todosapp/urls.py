1fa51a5b27d68cfb525139ce0402da803bedd154
Pablo Buitrago <pablo@briefboards.com>
2020-03-02 13:33:19 -0300

    cool

--- todosapp/urls.py
+++ todosapp/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^todos/', include('todos.urls', namespace='todos')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
